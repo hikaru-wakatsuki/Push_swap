c919e7c8549af3db0e9b1e97f6bb2bf96b55682b
Hikaru Wakatsuki <hwakatsu@c5r3s14.42tokyo.jp>
2026-01-21 20:32:31 +0900

    wak

--- src/main_util2.c
+++ src/main_util2.c
@@ -6,7 +6,7 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 20:43:29 by hwakatsu          #+#    #+#             */
-/*   Updated: 2026/01/21 20:28:44 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2026/01/21 20:31:50 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ bool	check_atoi(const char *nptr)
 		nbr += (long)(*nptr - '0');
 		nptr++;
 	}
-	if (*nptr != '\0' || !(ft_isdigit(*nptr)) || !(ft_isspace(*nptr)))
+	if (*nptr != '\0' && !(ft_isspace(*nptr)))
 		return (false);
 	nbr = nbr * sign;
 	if (nbr < INT_MIN || INT_MAX < nbr)
